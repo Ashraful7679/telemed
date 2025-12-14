2276c3637b46dd5411548ceed852fa4373f84c33
Ashraful Haque Sulaiman <group6bsakm@gmail.com>
2025-12-14 13:54:11 +0600

    Refactor next.config.js for cleaner configuration

--- next.config.js
+++ next.config.js
@@ -1,26 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  // Required for Netlify + Next.js
   output: "standalone",
-
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**.supabase.co",
-      },
-    ],
-  },
-
-  // Recommended for Windows dev stability
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.cache = false;
-    }
-    return config;
-  },
+        hostname: "**.supabase.co"
+      }
+    ]
+  }
 };
 
 module.exports = nextConfig;
